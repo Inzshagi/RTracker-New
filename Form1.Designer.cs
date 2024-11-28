7627888e13f54d8b57dc123285998d18f2260b94
Zyron <zyronbioalzate31@gmail.com>
2024-11-29 03:37:11 +0800

    Initial commit: Add RTracker project files

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace RTracker
 {
-    partial class Form1
+    partial class ToDoList
     {
         /// <summary>
         /// Required designer variable.
@@ -28,20 +28,204 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.titleTextBox = new System.Windows.Forms.TextBox();
+            this.descriptionTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.newButton = new System.Windows.Forms.Button();
+            this.editButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.toDoListView = new System.Windows.Forms.DataGridView();
+            this.dueDatePicker = new System.Windows.Forms.DateTimePicker();
+            this.dueDate = new System.Windows.Forms.Label();
+            this.statusComboBox = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Lucida Fax", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1132, 93);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "RTracker";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // titleTextBox
+            // 
+            this.titleTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.titleTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.titleTextBox.Location = new System.Drawing.Point(24, 140);
+            this.titleTextBox.Name = "titleTextBox";
+            this.titleTextBox.Size = new System.Drawing.Size(1120, 26);
+            this.titleTextBox.TabIndex = 1;
+            // 
+            // descriptionTextBox
+            // 
+            this.descriptionTextBox.BackColor = System.Drawing.SystemColors.Window;
+            this.descriptionTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.descriptionTextBox.Location = new System.Drawing.Point(24, 198);
+            this.descriptionTextBox.Name = "descriptionTextBox";
+            this.descriptionTextBox.Size = new System.Drawing.Size(275, 26);
+            this.descriptionTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label2.Location = new System.Drawing.Point(23, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Description: ";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label3.Location = new System.Drawing.Point(24, 114);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(1120, 23);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Title:";
+            // 
+            // newButton
+            // 
+            this.newButton.Location = new System.Drawing.Point(24, 242);
+            this.newButton.Name = "newButton";
+            this.newButton.Size = new System.Drawing.Size(275, 45);
+            this.newButton.TabIndex = 5;
+            this.newButton.Text = "New";
+            this.newButton.UseVisualStyleBackColor = true;
+            this.newButton.Click += new System.EventHandler(this.newButton_Click);
+            // 
+            // editButton
+            // 
+            this.editButton.Location = new System.Drawing.Point(307, 242);
+            this.editButton.Name = "editButton";
+            this.editButton.Size = new System.Drawing.Size(275, 45);
+            this.editButton.TabIndex = 6;
+            this.editButton.Text = "Edit";
+            this.editButton.UseVisualStyleBackColor = true;
+            this.editButton.Click += new System.EventHandler(this.editButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(588, 242);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(275, 45);
+            this.deleteButton.TabIndex = 7;
+            this.deleteButton.Text = "Delete";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(869, 242);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(275, 45);
+            this.saveButton.TabIndex = 8;
+            this.saveButton.Text = "Save";
+            this.saveButton.UseVisualStyleBackColor = true;
+            this.saveButton.Click += new System.EventHandler(this.saveButton_Click);
+            // 
+            // toDoListView
+            // 
+            this.toDoListView.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.toDoListView.BackgroundColor = System.Drawing.SystemColors.Window;
+            this.toDoListView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.toDoListView.GridColor = System.Drawing.Color.MediumSpringGreen;
+            this.toDoListView.Location = new System.Drawing.Point(24, 311);
+            this.toDoListView.Name = "toDoListView";
+            this.toDoListView.ReadOnly = true;
+            this.toDoListView.RowHeadersWidth = 51;
+            this.toDoListView.RowTemplate.Height = 24;
+            this.toDoListView.Size = new System.Drawing.Size(1119, 367);
+            this.toDoListView.TabIndex = 9;
+            // 
+            // dueDatePicker
+            // 
+            this.dueDatePicker.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dueDatePicker.Location = new System.Drawing.Point(588, 198);
+            this.dueDatePicker.Name = "dueDatePicker";
+            this.dueDatePicker.Size = new System.Drawing.Size(546, 26);
+            this.dueDatePicker.TabIndex = 10;
+            // 
+            // dueDate
+            // 
+            this.dueDate.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.dueDate.Location = new System.Drawing.Point(584, 172);
+            this.dueDate.Name = "dueDate";
+            this.dueDate.Size = new System.Drawing.Size(559, 23);
+            this.dueDate.TabIndex = 11;
+            this.dueDate.Text = "Due Date:";
+            // 
+            // statusComboBox
+            // 
+            this.statusComboBox.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.statusComboBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.statusComboBox.FormattingEnabled = true;
+            this.statusComboBox.Location = new System.Drawing.Point(307, 198);
+            this.statusComboBox.Name = "statusComboBox";
+            this.statusComboBox.Size = new System.Drawing.Size(275, 28);
+            this.statusComboBox.TabIndex = 12;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label4.Location = new System.Drawing.Point(306, 172);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(276, 23);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Status:";
+            // 
+            // ToDoList
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Name = "Form1";
-            this.Text = "x`x`x";
+            this.BackColor = System.Drawing.Color.SeaGreen;
+            this.ClientSize = new System.Drawing.Size(1156, 690);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.statusComboBox);
+            this.Controls.Add(this.dueDate);
+            this.Controls.Add(this.dueDatePicker);
+            this.Controls.Add(this.toDoListView);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.editButton);
+            this.Controls.Add(this.newButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.descriptionTextBox);
+            this.Controls.Add(this.titleTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "ToDoList";
+            this.Text = "RTracker";
+            this.Load += new System.EventHandler(this.ToDoList_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.toDoListView)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox titleTextBox;
+        private System.Windows.Forms.TextBox descriptionTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button newButton;
+        private System.Windows.Forms.Button editButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.DataGridView toDoListView;
+        private System.Windows.Forms.DateTimePicker dueDatePicker;
+        private System.Windows.Forms.Label dueDate;
+        private System.Windows.Forms.ComboBox statusComboBox;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
 
 namespace RTracker
 {
-    public partial class Form1 : Form
+    public partial class ToDoList : Form
     {
-        public Form1()
+        private SQLiteConnection connection;
+        private bool isEditing = false;
+
+        public ToDoList()
         {
             InitializeComponent();
+            string databasePath = @"C:\Users\Mark Angelo Alzate\source\repos\RTracker\Rtracker.db";
+            connection = new SQLiteConnection($"Data Source={databasePath};Version=3;");
+        }
+
+        private void ToDoList_Load(object sender, EventArgs e)
+        {
+            connection.Open();
+
+            //Add status items to the combo box
+            statusComboBox.Items.Add("Pending");
+            statusComboBox.Items.Add("In Progress");
+            statusComboBox.Items.Add("Completed");
+
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            string query = "SELECT Title, Description, DueDate, Status FROM TodoList";
+            SQLiteDataAdapter da = new SQLiteDataAdapter(query, connection);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            toDoListView.DataSource = dt;
+        }
+
+        private void saveButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(titleTextBox.Text) || string.IsNullOrWhiteSpace(descriptionTextBox.Text))
+            {
+                MessageBox.Show("Title and Description cannot be empty.");
+                return;
+            }
+
+            string query = isEditing
+                ? "UPDATE TodoList SET Title = @Title, Description = @Description, DueDate = @DueDate, Status = @Status WHERE Title = @OriginalTitle"
+                : "INSERT INTO TodoList (Title, Description, DueDate, Status) VALUES (@Title, @Description, @DueDate, @Status)"; // Include Status in the insert query
+
+            using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@Title", titleTextBox.Text);
+                cmd.Parameters.AddWithValue("@Description", descriptionTextBox.Text);
+                cmd.Parameters.AddWithValue("@DueDate", dueDatePicker.Value.ToString("yyyy-MM-dd"));
+                cmd.Parameters.AddWithValue("@Status", statusComboBox.SelectedItem.ToString()); 
+
+                if (dueDatePicker.Value.Date < DateTime.Now.Date)
+                {
+                    MessageBox.Show("Due Date cannot be in the past.");
+                    return;
+                }
+
+                if (isEditing)
+                {
+                    cmd.Parameters.AddWithValue("@OriginalTitle", titleTextBox.Text); // For updating existing task
+                }
+
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadData(); // Reload data to refresh DataGridView
+            titleTextBox.Clear();
+            descriptionTextBox.Clear();
+            statusComboBox.SelectedIndex = -1;
+            isEditing = false;
+        }
+
+        private void editButton_Click(object sender, EventArgs e)
+        {
+            if (toDoListView.CurrentRow == null)
+            {
+                MessageBox.Show("Select a row to edit.");
+                return;
+            }
+
+            isEditing = true;
+            titleTextBox.Text = toDoListView.CurrentRow.Cells[0].Value.ToString();
+            descriptionTextBox.Text = toDoListView.CurrentRow.Cells[1].Value.ToString();
+            dueDatePicker.Value = DateTime.Parse(toDoListView.CurrentRow.Cells[2].Value.ToString());
+        }
+
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if (toDoListView.CurrentRow == null)
+            {
+                MessageBox.Show("Select a row to delete.");
+                return;
+            }
+
+            string query = "DELETE FROM TodoList WHERE Title = @Title";
+            using (SQLiteCommand cmd = new SQLiteCommand(query, connection))
+            {
+                cmd.Parameters.AddWithValue("@Title", toDoListView.CurrentRow.Cells[0].Value.ToString());
+                cmd.ExecuteNonQuery();
+            }
+
+            LoadData(); // Reload data to refresh DataGridView
+        }
+
+        private void newButton_Click(object sender, EventArgs e)
+        {
+            titleTextBox.Clear();
+            descriptionTextBox.Clear();
+            isEditing = false;
+        }
+
+        private void ToDoList_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            connection.Close();
         }
     }
 }
